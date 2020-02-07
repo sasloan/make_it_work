b1cfba3b3e1f1f9635c0cec23fa6a62cd6aa06da
sasloan <sebastiansloan94@gmail.com>
2020-02-07 10:15:17 -0700

    Wrote conetestant count spec

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -6,6 +6,26 @@ describe 'As a Visitor' do
 			@outfits = Challenge.create!(theme: "Trash Wear", project_budget: 350)
 			@denim = @outfits.projects.create!(name: "Denim Trash Wear", material: "Denim")
 			@pink = @outfits.projects.create!(name: "Pinky Due", material: "Satin")
+			@glam = @outfits.projects.create!(name: "Glam Style", material: "Silk")
+			@accessories = Challenge.create!(theme: "Using Accessories", project_budget: 200)
+			@hat = @accessories.projects.create!(name: "Hat style", material: "Hat stuff")
+			@shoe = @accessories.projects.create!(name: "Shoe styles", material: "shoe stuff")
+			@holly = Contestant.create!(name: "Holly", age: 27, hometown: "Detroit", years_of_experience: 3)
+			@nancy = Contestant.create!(name: "Nancy", age: 29, hometown: "Arvada", years_of_experience: 5)
+			@sherry = Contestant.create!(name: "Sherry", age: 28, hometown: "Westminster", years_of_experience: 2)
+			@tammi = Contestant.create!(name: "Tammi", age: 25, hometown: "Thornton", years_of_experience: 1)
+
+			@holly.projects << @denim
+			@holly.projects << @shoe
+
+			@nancy.projects << @glam
+			@nancy.projects << @hat
+			@nancy.projects << @pink
+
+			@sherry.projects << @pink
+			@sherry.projects << @shoe
+
+			@tammi.projects << @denim
 		end
 
 		it 'I see the projects name and material' do
@@ -25,6 +45,13 @@ describe 'As a Visitor' do
 			expect(current_path).to eq("/projects/#{@denim.id}")
 
 			expect(page).to have_content(@outfits.theme)
+		end
+
+		it "I can see a count of the contestants on this project" do
+
+			visit "/projects/#{@denim.id}"
+
+			expect(page).to have_content("Total Contestants: 2")
 		end
 	end
 end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,36 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+	describe "methods" do
+		before :each do
+			@outfits = Challenge.create!(theme: "Trash Wear", project_budget: 350)
+			@denim = @outfits.projects.create!(name: "Denim Trash Wear", material: "Denim")
+			@pink = @outfits.projects.create!(name: "Pinky Due", material: "Satin")
+			@glam = @outfits.projects.create!(name: "Glam Style", material: "Silk")
+			@accessories = Challenge.create!(theme: "Using Accessories", project_budget: 200)
+			@hat = @accessories.projects.create!(name: "Hat style", material: "Hat stuff")
+			@shoe = @accessories.projects.create!(name: "Shoe styles", material: "shoe stuff")
+			@holly = Contestant.create!(name: "Holly", age: 27, hometown: "Detroit", years_of_experience: 3)
+			@nancy = Contestant.create!(name: "Nancy", age: 29, hometown: "Arvada", years_of_experience: 5)
+			@sherry = Contestant.create!(name: "Sherry", age: 28, hometown: "Westminster", years_of_experience: 2)
+			@tammi = Contestant.create!(name: "Tammi", age: 25, hometown: "Thornton", years_of_experience: 1)
+
+			@holly.projects << @denim
+			@holly.projects << @shoe
+
+			@nancy.projects << @glam
+			@nancy.projects << @hat
+			@nancy.projects << @pink
+
+			@sherry.projects << @pink
+			@sherry.projects << @shoe
+
+			@tammi.projects << @denim
+		end
+
+		it '#count' do
+			expect(@denim.count).to eq(2)
+		end
+	end
 end
